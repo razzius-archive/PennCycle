9e379f576270b054c699fc434d87b94ff3d9655f
Kevin Shen <kwshen@gmail.com>
2012-01-26 21:18:38 -0500

    added bike checkin functionality

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.db.models import Q
+import datetime
 
 # Create your models here.
+day_end = datetime.time(23, 0, 0)
+day_start = datetime.time(9, 0, 0)
 
 GENDER_CHOICES = (
     ('M', 'Male'),
@@ -46,7 +50,7 @@ class User(models.Model):
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     grad_year = models.CharField(max_length=5, choices=GRAD_YEAR_CHOICES)
     join_date = models.DateField(null=True, blank=True)
-    height = models.IntegerField(max_length=2)
+    height = models.CharField(max_length=10)
     school = models.CharField(max_length=10, choices=SCHOOL_CHOICES)
 
     def __unicode__(self):
@@ -59,26 +63,56 @@ class Bike(models.Model):
     manufacturer = models.ForeignKey(Manufacturer)
     purchase_date = models.DateField()
     color = models.CharField(max_length=30, blank=True)
-    top_bar = models.CharField(max_length=20, blank=True)
-    problems = models.CharField(max_length=300, blank=True)
+    #top_bar = models.CharField(max_length=20, blank=True)
+    #problems = models.CharField(max_length=300, blank=True)
     #bike_model = models.CharField(max_length=100, blank=True)
+    status = models.CharField(max_length=100, default='available')
 
     # added availibility field
-    available = models.BooleanField()
+    #available = models.BooleanField()
+    #@property
+    #def status(self):
+    #  ride = self.rides.objects.order_by('checkout_time')[0]
+    #  if ride.checkin_time != None:
+    #    status = 'out'
+    #  elif ride.checkin_time < datetime.datetime.now():
+    #    status = 'available'
+    #  else:
+    #    status = 'error'
+    #  return status
+
 
     def __unicode__(self):
         return self.bike_name
 
 class Ride(models.Model):
     rider = models.ForeignKey(User)
-    bike = models.ForeignKey('Bike', limit_choices_to = {'available': True})
-    checkout_time = models.DateTimeField()
+    bike = models.ForeignKey('Bike', limit_choices_to = {'status': 'available'},
+      related_name='rides')
+    checkout_time = models.DateTimeField(default=datetime.datetime.now())
     checkin_time = models.DateTimeField(null=True, blank=True)
-    ride_duration_days = models.IntegerField()
+    #ride_duration_days = models.IntegerField()
+    @property
+    def ride_duration_days(self):
+      if self.checkin_time == None:
+        end = datetime.datetime.now()
+      else:
+        end = self.checkin_time
+      duration = end - self.checkout_time
+      duration_days = duration.days 
+      return duration_days
     
     #added list of bikes
     #bike = Bike.objects.filter(availible=True)
 
+    def save(self):
+      super(Ride, self).save()
+      if self.checkin_time == None:
+        self.bike.status = 'out'
+      else:
+        self.bike.status = 'available'
+      self.bike.save()
+     
     def __unicode__(self):
         return u'%s on %s' % (self.rider, self.checkout_time)
 
