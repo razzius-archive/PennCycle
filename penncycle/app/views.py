bcb993e8f1012833f1230badb56bf24df5ef5144
Kevin Shen <kwshen@gmail.com>
2012-03-12 11:34:23 -0400

    changed verify to GET'

--- penncycle/app/views.py
+++ penncycle/app/views.py
@@ -96,23 +96,23 @@ def signup(request):
   return render_to_response('signup.html', context_instance)
 
 def verify_payment(request):
-  if request.method == 'POST':
-    print "received POST from pay server"
+  if request.method == 'GET':
+    print "received GET from pay server"
     # gets the student with penncard specified in POST data
-    stu = Student.objects.get(Penncard=request.POST.get('ordernumber'))
+    stu = Student.objects.get(Penncard=request.GET.get('ordernumber'))
 
     # generate token from penncard number and shared password
-    token = hmac.new("uPENNBIK3S!", request.POST.get('ordernumber'), hashlib.sha256).hexdigest()
+    token = hmac.new("uPENNBIK3S!", request.GET.get('ordernumber'), hashlib.sha256).hexdigest()
 
     # if token matches with token from CyberPay, payment completed
-    if token == request.POST.get('token'):
+    if token == request.GET.get('token') and int(request.GET.get('approval')) == 1:
       stu.paid = True
       stu.save()
       return render_to_response('thanks.html', {})
     else:
       return render_to_response('paymentfailed.html', {})
   else:
-    return HttpResponse("This was a GET") 
+    return HttpResponse("This was not a GET") 
 
 '''
 def payment(request):
