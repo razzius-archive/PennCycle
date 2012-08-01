4ee347d2c84f6313f1c14a55643f89b7765e3e50
Alex Rattray <rattray.alex@gmail.com>
2012-08-01 16:47:45 -0700

    stations, payments better

--- penncycle/app/models.py
+++ penncycle/app/models.py
@@ -123,24 +123,19 @@ class Student(models.Model):
   payment_type = models.CharField(max_length=100, choices=PAYMENT_CHOICES, blank=True, null=True)
   at_desk = models.NullBooleanField()
   plan = models.ManyToManyField('Plan', blank=True, null=True)
-  paid_now = lambda self: self.payments.filter(
-    satisfied=True,
-    plan__start_date__lte=today,
-    plan__end_date__gte=today,
-    )
 
-  # @property
-  # def paid_now(self):
-  #   today = datetime.date.today()
-  #   payments = self.payments.filter(
-  #     satisfied=True,
-  #     plan__start_date__lte=today,
-  #     plan__end_date__gte=today,
-  #     )
-  #   if len(payments) > 0:
-  #     return True
-  #   else:
-  #     return False
+  @property
+  def paid_now(self):
+    today = datetime.date.today()
+    payments = self.payments.filter(
+      satisfied=True,
+      plan__start_date__lte=today,
+      plan__end_date__gte=today,
+      )
+    if len(payments) > 0:
+      return True
+    else:
+      return False
 # 
   def can_ride(self):
     if self.status == 'available' and self.waiver_signed == True and self.paid_now == True:
