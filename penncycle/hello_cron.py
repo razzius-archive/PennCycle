5b4c58e98134915b2941b983c65b7c433149b306
Alex Rattray <rattray.alex@gmail.com>
2012-08-02 13:43:49 -0700

    trying to insert path to hello_cron

--- penncycle/hello_cron.py
+++ penncycle/hello_cron.py
@@ -1,3 +1,12 @@
+import os
+from os.path import abspath, dirname
+import sys
+path = '~/penncycle'
+if path not in sys.path:
+    sys.path.append(path)
+sys.path.insert(0, path)
+os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
+
 from app.models import *
 from app.views import email_alex
 
