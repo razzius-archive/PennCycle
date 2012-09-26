7fa7805b65306fed319bcb5389ff8d4c32762b1a
Alex Rattray <rattray.alex@gmail.com>
2012-09-26 16:12:57 -0400

    better filtering of riders in rides

--- penncycle/app/admin_stuff.py
+++ penncycle/app/admin_stuff.py
@@ -46,13 +46,13 @@ class RidesAdmin(admin.ModelAdmin):
       'rider', 'bike', 'checkout_time', 'checkin_time', 'ride_duration_days', 'status', 'checkin_station',
   )
   list_filter = (
-      'rider', 'bike', 'checkout_time', 'checkin_time', 'checkin_station',
+      'bike', 'checkout_time', 'checkin_time', 'checkin_station', 'checkout_station',
   ) 
   readonly_fields = ('ride_duration_days', 'num_users')
   date_hierarchy = 'checkin_time'
   ordering = ('-checkout_time',)
   actions = ['check_in']
-  search_fields = ['rider__name','rider__penncard','bike__bike_name']
+  search_fields = ['rider__name','rider__penncard','bike__bike_name', 'checkin_station', 'checkout_stations']
   save_on_top = True
 
   @csrf_protect_m
