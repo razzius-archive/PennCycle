44c3a28db75fbedd489f8d99c33cce0e8d734993
Razzi Abuissa <razzi53@gmail.com>
2013-03-10 21:33:34 -0500

    Removed old Twilio, worked on stat command

--- penncycle/app/management/commands/stat.py
+++ penncycle/app/management/commands/stat.py
@@ -1,6 +1,16 @@
+import datetime
 from django.core.management.base import NoArgsCommand
 from app.models import *
 
 class Command(NoArgsCommand):
 	def handle_noargs(self, **options):
-		print("Hi.")
+		today = datetime.datetime.today()
+		day = today.day
+		week_ago_day = 0 if day < 7 else day - 7
+		if week_ago_day==0:
+			print("Since the start of the month:")
+		else:
+			print("Since a week ago:")
+		week_ago = datetime.datetime.replace(today, day=week_ago_day)
+		students = Student.objects.filter(join_date__gt=week_ago).count()
+		print(students)
