1fd3ae2586d160b923f03eac7ccae4e17466a900
Razzi <razzi53@gmail.com>
2013-08-11 16:50:07 -0400

    signup_form

--- penncycle/util/util.py
+++ penncycle/util/util.py
@@ -1,5 +1,16 @@
 from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
+from django_twilio.client import twilio_client
+
+
+def send_pin_to_student(student):
+    pin = student.pin
+    twilio_client.sms.messages.create(
+        to=student.twilio_phone,
+        body="Your PIN for PennCycle is {}."
+        " Log in the website at penncycle.org/signin.".format(pin),
+        from_="+12156885468"
+    )
 
 def email_razzi(message):
     send_mail(
