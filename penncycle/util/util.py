e2b9283e297709d24e8ac95dea360b1d9f8783c0
Razzi <razzi53@gmail.com>
2013-08-24 17:01:55 -0400

    try send pin;

--- penncycle/util/util.py
+++ penncycle/util/util.py
@@ -4,13 +4,16 @@ from django_twilio.client import twilio_client
 
 
 def send_pin_to_student(student):
-    pin = student.pin
-    twilio_client.sms.messages.create(
-        to=student.twilio_phone,
-        body="Your PIN for PennCycle is {}."
-        " Log in the website at penncycle.org/signin.".format(pin),
-        from_="+12156885468"
-    )
+    try:
+        pin = student.pin
+        twilio_client.sms.messages.create(
+            to=student.twilio_phone,
+            body="Your PIN for PennCycle is {}."
+            " Log in the website at penncycle.org/signin.".format(pin),
+            from_="+12156885468"
+        )
+    except Exception as error:
+        email_razzi("Pin send failed: {}".format(error))
 
 def email_razzi(message):
     send_mail(
