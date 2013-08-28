c2635fc713169c821d0cb6c0615d116133d9b50f
Razzi <razzi53@gmail.com>
2013-08-28 00:09:40 -0400

    mobile api for bike and station data

---
+++ penncycle/assets/js/map.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+  var map_options = {
+    center: new google.maps.LatLng(39.951600,-75.197794),
+    zoom: 17,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoomControl: true
+  };
+  var station_counts = {};
+  var map = new google.maps.Map(document.getElementById("map"), map_options);
+  $.getJSON("/mobile/bike_data/", function(bikes) {
+    for (var i = bikes.length - 1; i >= 0; i--) {
+      bike = bikes[i];
+      if (bike.status == "available") {
+        lat = bike.latitude - 0.0002;
+        lng = bike.longitude - 0.0001;
+        station = bike.location;
+        if (station_counts[station]) {
+          lng += 0.0002 * (station_counts[station] % 5);
+          lat -= 0.0002 * Math.floor(station_counts[station] / 5);
+          station_counts[station] += 1;
+        } else {
+          station_counts[station] = 1;
+        }
+        pos = new google.maps.LatLng(lat, lng);
+        var marker = new MarkerWithLabel({
+          position: pos,
+          map: map,
+          icon: "/static/img/bike_icon.png",
+          labelContent: bike.name,
+          labelClass: "marker"
+        });
+      }
+    }
+  });
+  $.getJSON("/mobile/station_data/", function(stations) {
+    for (var i = stations.length - 1; i >= 0; i--) {
+      station = stations[i];
+      lng = station.longitude;
+      lat = station.latitude;
+      pos = new google.maps.LatLng(lat, lng);
+      var marker = new MarkerWithLabel({
+        position: pos,
+        map: map,
+        icon: "/static/img/station_icon.png",
+        labelContent: station.name,
+        labelClass: "marker"
+      });
+    }
+  });
+});
