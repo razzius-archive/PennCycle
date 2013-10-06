1bdc578f1156367cc0167013f2b53d08bf1b516d
Razzi <razzi53@gmail.com>
2013-10-06 17:05:01 -0400

    removed user and password for local postgres

--- penncycle/settings/local.py
+++ penncycle/settings/local.py
@@ -3,26 +3,16 @@ from base import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'pcpg',
-#         'USER': 'razzi',                      # Not used with sqlite3.
-#         'PASSWORD': 'razzi',                  # Not used with sqlite3.
-#         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'default.db',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pcpg',
         'USER': '',
         'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 STATIC_URL = '/static/'
